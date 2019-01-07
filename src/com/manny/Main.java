9033ddddb1cad141a8f4a1f4eadf874fd4e1d429
Manny Aguilar <mannyaaguilar@yahoo.com>
2019-01-07 01:10:41 -0700

    first commit

---
+++ src/com/manny/Main.java
@@ -0,0 +1,20 @@
+package com.manny;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        for (int i = 2; i <= 100; i++) {
+            for(int j = 2; j<=i; j++){
+                if(j==i){
+                    System.out.println(i);
+                }
+                if(i%j==0){
+                    break;
+                }
+
+            }
+
+        }
+    }
+}
